ef5064c86667dc3efc2ae096ca9ac2a29ff9ab3f
yverdev <contact@yverdev.com>
2020-06-04 21:52:30 +0200

    code update

--- app.js
+++ app.js
@@ -32,9 +32,45 @@
     
     let randomNumber = Math.floor(Math.random()*100) + 1;
 
+    function checkGuess(){
+        
+        let userGuess = Number(guessField.value);
+        if (guessCount === 1) {
+          guesses.textContent = 'Propositions précédentes : ';
+        }
+        guesses.textContent += userGuess + ' ';
+       
+        if (userGuess === randomNumber) {
+          lastResult.textContent = 'Bravo, vous avez trouvé le nombre !';
+          lastResult.style.backgroundColor = 'green';
+          lowOrHi.textContent = '';
+          setGameOver();
+        } else if (guessCount === 10) {
+           lastResult.textContent = '!!! PERDU !!!';
+           setGameOver();
+        } else {
+           lastResult.textContent = 'Faux !';
+           lastResult.style.backgroundColor = 'red';
+           if (userGuess < randomNumber) {
+            lowOrHi.textContent = 'Le nombre saisi est trop petit !';
+           } else if (userGuess > randomNumber) {
+            lowOrHi.textContent = 'Le nombre saisi est trop grand !';
+           }
+        }
+       
+        guessCount++;
+        guessField.value = '';
+        guessField.focus();
+      }
 
-
-
+      function setGameOver() {
+        guessField.disabled = true;
+        guessSubmit.disabled = true;
+        resetButton = document.createElement('button');
+        resetButton.textContent = 'Start new game';
+        document.body.appendChild(resetButton);
+        resetButton.addEventListener('click', resetGame);
+      }
 
 
 
